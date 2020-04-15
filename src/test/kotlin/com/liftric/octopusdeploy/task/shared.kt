a6d06ee6373104538d53e36121d73a4dd110fcd7
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2020-04-15 11:49:28 +0200

    feat(CI): build as machine

--- src/test/kotlin/com/liftric/octopusdeploy/task/shared.kt
+++ src/test/kotlin/com/liftric/octopusdeploy/task/shared.kt
@@ -5,14 +5,20 @@ import junit.framework.TestCase
 import java.io.File
 
 fun File.setupGitRepo() {
-    this.shell("git init .").exitCode mustBe 0
-    this.shell("git add .").exitCode mustBe 0
-    this.shell("git commit -m \"initial commit\"").exitCode mustBe 0
-    this.shell("git tag first-one").exitCode mustBe 0
-    this.shell("touch secondfile").exitCode mustBe 0
-    this.shell("git add .").exitCode mustBe 0
-    this.shell("git commit -m \"second commit\"").exitCode mustBe 0
-    this.shell("git tag second-one").exitCode mustBe 0
+    try {
+        this.shell("git init .").exitCode mustBe 0
+        this.shell("git add .").exitCode mustBe 0
+        this.shell("git commit -m \"initial commit\"").exitCode mustBe 0
+        this.shell("git tag first-one").exitCode mustBe 0
+        this.shell("touch secondfile").exitCode mustBe 0
+        this.shell("git add .").exitCode mustBe 0
+        this.shell("git commit -m \"second commit\"").exitCode mustBe 0
+        this.shell("git tag second-one").exitCode mustBe 0
+    } catch (e: Exception) {
+        println(e.message)
+        e.printStackTrace()
+        throw e
+    }
 }
 
 infix fun Int.mustBe(expected: Int) {
