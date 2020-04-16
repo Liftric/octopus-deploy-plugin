64cd01539cd79cc7bb53ede48cc3e1f035c88f93
Marcel Kesselring <Ingwersaft@users.noreply.github.com>
2020-04-16 10:27:05 +0200

    fix(gradle): set source and target compatibility to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,10 @@ dependencies {
     integrationTestImplementation("junit:junit:4.12")
     integrationTestImplementation("org.apache.httpcomponents:httpclient:4.5.12")
 }
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
